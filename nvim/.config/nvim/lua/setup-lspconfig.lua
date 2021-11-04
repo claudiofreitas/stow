2c830e9a4c4116640895b152d8b5cd6c5791419d
Claudio Freitas <claudiofreitas3@gmail.com>
2021-11-05 02:03:30 +0900

    nvim: Add some tsserver config to experiment

--- nvim/.config/nvim/lua/setup-lspconfig.lua
+++ nvim/.config/nvim/lua/setup-lspconfig.lua
@@ -15,7 +15,7 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-lspconfig.sumneko_lua.setup {
+lspconfig.sumneko_lua.setup({
   cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"},
   settings = {
     Lua = {
@@ -34,7 +34,7 @@ lspconfig.sumneko_lua.setup {
       },
     }
   }
-}
+})
 
 
 
@@ -48,4 +48,43 @@ require('nlua.lsp.nvim').setup(require('lspconfig'), {
   }
 })
 
+
+
 lspconfig.tsserver.setup({})
+
+
+local on_attach = function(_, bufnr)
+  local function local_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  local function local_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
+
+  local_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local opts = { noremap=true, silent=true }
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  -- https://github.com/neovim/nvim-lspconfig
+  local_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  local_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  local_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  local_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  local_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  local_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  local_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  local_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+local servers = { 'tsserver' }
+for _, server in ipairs(servers) do
+  lspconfig[server].setup({
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150, -- <- What does this do?
+    }
+  })
+end
+
+
+print("loaded setup-lspconfig.lua")
