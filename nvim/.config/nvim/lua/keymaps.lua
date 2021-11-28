b870ee28c1be10ae3f0f5a1be9d9e8ee27ef4aed
Claudio Freitas <claudiofreitas3@gmail.com>
2021-11-28 17:59:34 +0900

    nvim: keymap to open CHADtree

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,9 +1,9 @@
---vim.api.nvim_set_keymap('i', '<Tab>',   'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
---vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
-
+-- General
 vim.api.nvim_set_keymap('i', '<Tab>',   '"\\<C-n>"', {expr = true})
 vim.api.nvim_set_keymap('i', '<S-Tab>', '"\\<C-p>"', {expr = true})
 vim.api.nvim_set_keymap("n", "<F1>", ':lua vim.lsp.buf.signature_help() <CR>', {})
+
+
 
 -- Telescope
 vim.api.nvim_set_keymap("n", "<Leader>o", "<cmd>lua require('telescope.builtin').find_files()<cr>", {})
@@ -13,20 +13,22 @@ vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>lua require('telescope.builtin')
 vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", {})
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').file_browser()<cr>", {})
 vim.api.nvim_set_keymap("n", "<leader><cr>", "<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>", {})
+
 -- Actually the keymap below is CTRL+/, but I don't know why it should be mapped as _
 vim.api.nvim_set_keymap("n", "<C-_>", ":Telescope current_buffer_fuzzy_find <CR>", {})
 
--- Git Fugitive
+
+
+-- Fugitive
 vim.api.nvim_set_keymap("n", "<C-g>", ":G <CR>", {})
 
+
+
+-- CHADtree
+vim.api.nvim_set_keymap("n", "<Leader>1", ":CHADopen <CR>", {})
+
+
+
+-- Misc
 vim.api.nvim_command('nmap <F7> :TSHighlightCapturesUnderCursor<CR>')
 
-
-
---vim.cmd [[
---  inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
---  inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
---]]
---vim.cmd [[
---  nnoremap <Tab> :bn<CR>
---]]
