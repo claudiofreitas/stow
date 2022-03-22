7215fdc46ce6860224d1b2e8d5a81f3a3b77b4ff
Claudio Freitas <claudiofreitas3@gmail.com>
2022-03-22 22:24:53 +0900

    nvim: change style of lua code

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,24 +2,24 @@
 require('packer').startup(
   function(use)
     -- Packer
-    use 'wbthomason/packer.nvim'
+    use('wbthomason/packer.nvim')
 
-    use { 
+    use({
       'nvim-treesitter/nvim-treesitter',
       run = ':TSUpdate',
-    }
-        
-    use 'nvim-treesitter/playground'
+    })
+
+    use('nvim-treesitter/playground')
 
     -- Legend of Zelda theme
-    use 'lucastrvsn/kikwis'
+    use('lucastrvsn/kikwis')
 
     -- Sumneko in my repo:
-    use 'claudiofreitas/nlua.nvim'
+    -- use('claudiofreitas/nlua.nvim')
 
     --" (OPTIONAL): If you want to use built-in LSP (requires Neovim HEAD)
     --"   Currently only supported LSP, but others could work in future if people send PRs :)
-    use 'neovim/nvim-lspconfig'
+    use('neovim/nvim-lspconfig')
 
     --" (OPTIONAL): This is a suggested plugin to get better Lua syntax highlighting
     --"   but it's not currently required
@@ -28,60 +28,69 @@ require('packer').startup(
     --" (OPTIONAL): If you wish to have fancy lua folds, you can check this out.
     -- use 'tjdevries/manillua.nvim'
 
-    use 'nvim-lua/plenary.nvim'
+    use('nvim-lua/plenary.nvim')
 
     --use 'Th3Whit3Wolf/onebuddy'
     --use '~/.config/nvim/pack/claudiocolor'
     --use 'marko-cerovac/material.nvim'
-    use 'rktjmp/lush.nvim'
-    use '~/repos/claudio-darker.lua'
-    
-    -- Telescope
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
-    }
+    use('rktjmp/lush.nvim')
+    use({
+      '~/repos/claudio-darker.lua',
+      -- config = "vim.cmd('colorscheme claudio-darker')",
+    })
 
-    use 'kyazdani42/nvim-web-devicons'
+    -- Telescope
+    use({
+      'nvim-telescope/telescope.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+    })
+
+    use('kyazdani42/nvim-web-devicons')
 
     use('tpope/vim-fugitive')
 
-    use {
+    use({
       'lewis6991/gitsigns.nvim',
-      requires = {
-        'nvim-lua/plenary.nvim'
-      },
-    }
+      requires = { 'nvim-lua/plenary.nvim' },
+    })
 
-    use { "hrsh7th/nvim-cmp" }
-    use { 'ms-jpq/chadtree' }
+    use('hrsh7th/cmp-nvim-lsp')
+    use('hrsh7th/cmp-buffer')
+    use('hrsh7th/cmp-path')
+    use('hrsh7th/cmp-cmdline')
+    use('hrsh7th/nvim-cmp')
+
+    use('ms-jpq/chadtree')
 
   end
 )
 
-require('cmp').setup({
-  enabled = true,
-  autocomplete = true,
-  debug = false,
-  min_length = 1,
-  preselect = "enable",
-  throttle_time = 80,
-  source_timeout = 200,
-  incomplete_delay = 400,
-  max_abbr_width = 100,
-  max_kind_width = 100,
-  max_menu_width = 100,
-  source = {
-    path = true,
-    buffer = true,
-    calc = true,
-    vsnip = true,
-    nvim_lsp = true,
-    nvim_lua = true,
-    spell = true,
-    tags = true,
-    snippets_nvim = true,
-    treesitter = true
-  }
-})
+
+require('cmp').setup({})
+
+-- require('cmp').setup({
+--   enabled = true,
+--   autocomplete = true,
+--   debug = false,
+--   min_length = 1,
+--   preselect = "enable",
+--   throttle_time = 80,
+--   source_timeout = 200,
+--   incomplete_delay = 400,
+--   max_abbr_width = 100,
+--   max_kind_width = 100,
+--   max_menu_width = 100,
+--   source = {
+--     path = true,
+--     buffer = true,
+--     calc = true,
+--     vsnip = true,
+--     nvim_lsp = true,
+--     nvim_lua = true,
+--     spell = true,
+--     tags = true,
+--     snippets_nvim = true,
+--     treesitter = true
+--   }
+-- })
 
