baea622fc9eff230ddde7fb99159865e54c323f1
Claudio Freitas <claudiofreitas3@gmail.com>
2022-07-06 21:09:52 +0900

    tmux: Fix dotfiles-status.sh script.
    git command should have --git-dir and --work-tree together even when not
    using the git worktree in different places.

--- tmux/.config/tmux/dotfiles-status.sh
+++ tmux/.config/tmux/dotfiles-status.sh
@@ -1,6 +1,6 @@
 print_status() {
   local STATUS
-  STATUS=$(git --work-tree=/Users/claudio/stow status --porcelain --ignore-submodules 2> /dev/null | tail -n1)
+  STATUS=$(git --git-dir=/Users/claudio/stow/.git --work-tree=/Users/claudio/stow status --porcelain --ignore-submodules 2> /dev/null | tail -n1)
   if [[ -n $STATUS ]]
   then
     echo '#[fg=#282c34,bg=#e06c75] Stow:  '
