c98521da08728ed56a47911ed89a4a20ba0b27c2
Claudio Freitas <claudiofreitas3@gmail.com>
2022-08-27 19:26:19 +0900

    nvim: change the syntax limit of 50 chars to 80 chars for commit messages
    Also unset the textwidth that would make autowrap to next line based on some default number.

---
+++ nvim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_command("setlocal textwidth=0")
+-- vim.api.nvim_command("setlocal colorcolumn=+1")
---
+++ nvim/.config/nvim/after/syntax/gitcommit.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_command([[syn clear gitcommitSummary]])
+vim.api.nvim_command([[syn match   gitcommitSummary    "^.*\%<80v." contained containedin=gitcommitFirstLine nextgroup=gitcommitOverflow contains=@Spell]])
+vim.api.nvim_command([[highlight def link gitcommitOverflow Error]])
