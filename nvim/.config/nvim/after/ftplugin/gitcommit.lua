d95f05618257a99af913f435e690726774d9b235
Claudio Freitas <claudiofreitas3@gmail.com>
2022-08-27 19:29:16 +0900

    nvim: include stackoverflow link to future tweak

--- nvim/.config/nvim/after/ftplugin/gitcommit.lua
+++ nvim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -1,2 +1,4 @@
-vim.api.nvim_command("setlocal textwidth=0")
--- vim.api.nvim_command("setlocal colorcolumn=+1")
+-- This answer has some other interesting tweaks that are possible:
+-- https://stackoverflow.com/a/43953491/1369621
+
+vim.api.nvim_command('setlocal textwidth=0')
