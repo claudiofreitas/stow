faa6d6690ee7ef1b7f6c4f154a0485ba826bd26c
Claudio Freitas <claudiofreitas3@gmail.com>
2022-11-23 22:50:19 +0900

    zsh: change gl (git log) alias to use a new script that pretty prints the columns (from Thorsten Ball github.com/mrnugget)

---
+++ bin/.local/bin/pretty_git_log.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+HASH="%C(always,yellow)%h%C(always,reset)"
+# RELATIVE_TIME="%C(always,green)%ar%C(always,reset)"
+AUTHOR_DATETIME="%C(always,green)%ai%C(always,reset)"
+AUTHOR="%C(always,blue)%an%C(always,reset)"
+REFS="%C(always,red)%d%C(always,reset)"
+SUBJECT="%s"
+
+# FORMAT="$HASH $RELATIVE_TIME{$AUTHOR{$REFS $SUBJECT"
+FORMAT="$HASH $AUTHOR_DATETIME{$AUTHOR{$REFS $SUBJECT"
+
+pretty_git_log() {
+  git log --pretty="tformat:$FORMAT" $* |
+    column -t -s '{' -c 60 |
+    less -XRS --quit-if-one-screen
+}
+
+# From: https://github.com/mrnugget/dotfiles/blob/master/githelpers
+# Help: https://www.git-scm.com/docs/git-log
+
