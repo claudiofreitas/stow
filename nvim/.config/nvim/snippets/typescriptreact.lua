e3e779e463ff0c6fa409b827a1c4fada9ee94a29
Claudio Freitas <claudiofreitas3@gmail.com>
2022-12-09 01:42:43 +0900

    nvim: create React snippets for FC and Jest 'it' block

---
+++ nvim/.config/nvim/snippets/typescriptreact.lua
@@ -0,0 +1,65 @@
+local luasnip = require('luasnip')
+
+local snippet = luasnip.s
+local ins_node = luasnip.i
+-- local text_node = luasnip.t
+-- local dynamic_node = luasnip.dynamic_node
+-- local choice_node = luasnip.choice_node
+-- local function_node = luasnip.function_node
+-- local snippet_node = luasnip.snippet_node
+
+local format = require('luasnip.extras.fmt').fmt
+local rep_node = require('luasnip.extras').rep
+
+local snippets, autosnippets = {}, {}
+
+local function create_snippet(context, nodes, opts)
+	local new_snippet = snippet(context, nodes, opts)
+	table.insert(snippets, new_snippet)
+end
+
+create_snippet(
+	{
+		trig = 'FC (React Functional Component)',
+		name = 'React Functional Component',
+		dscr = 'Expands to a React functional component declaration',
+	},
+	format(
+		[[
+interface Props extends ComponentProps<{}> {{}}
+
+const {}: FC<Props> = () => {{
+  return (
+    <></>
+  );
+}};
+
+export {{ {} }};
+]],
+		{
+			ins_node(1, 'typeof InnerComponent'),
+			ins_node(2, 'ComponentName'),
+			rep_node(2),
+		}
+	)
+)
+
+create_snippet(
+	{
+		trig = "it('should",
+		name = "Jest it('should ...') block",
+		dscr = 'Expands to a Jest it case scenario',
+	},
+	format(
+		[[
+it('should TODO', async () => {{
+  {}
+}});
+]],
+		{
+			ins_node(0),
+		}
+	)
+)
+
+return snippets, autosnippets
