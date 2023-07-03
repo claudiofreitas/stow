a5b7b94781a2d61347d12f0bd24e10cd2aa5fe10
Claudio Freitas <claudiofreitas3@gmail.com>
2023-07-03 15:04:40 +0900

    nvim: fix lspsaga configuration

--- nvim/.config/nvim/lua/setup-lspsaga.lua
+++ nvim/.config/nvim/lua/setup-lspsaga.lua
@@ -1,3 +1,32 @@
+-- https://github.com/nvimdev/lspsaga.nvim
+
 local lspsaga = require('lspsaga')
 
-lspsaga.init_lsp_saga()
+lspsaga.setup({
+
+	ui = {
+		-- This option only works in Neovim 0.9
+		title = true,
+		-- Border type can be single, double, rounded, solid, shadow.
+		border = 'single',
+		winblend = 0,
+		expand = '',
+		collapse = '',
+		code_action = '💡',
+		incoming = ' ',
+		outgoing = ' ',
+		hover = ' ',
+		kind = {},
+	},
+
+	symbol_in_winbar = {
+		enable = true,
+		separator = '  ',
+		ignore_patterns = {},
+		hide_keyword = false,
+		show_file = true,
+		folder_level = 1,
+		respect_root = false,
+		color_mode = true,
+	},
+})
