0b00958a2d0e57e804137a3936f002155095e824
Claudio Freitas <claudiofreitas3@gmail.com>
2023-07-05 18:27:31 +0900

    nvim: install and configure (WIP/trying) Noice.nvim

--- nvim/.config/nvim/lua/setup-lazy.lua
+++ nvim/.config/nvim/lua/setup-lazy.lua
@@ -112,6 +112,52 @@ require('lazy').setup({
 			options = { try_as_border = true },
 		},
 	},
+	{
+		'folke/noice.nvim',
+		event = 'VeryLazy',
+		opts = {
+			-- https://github.com/folke/noice.nvim#%EF%B8%8F-configuration
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+					['vim.lsp.util.stylize_markdown'] = true,
+					['cmp.entry.get_documentation'] = true,
+				},
+			},
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+			routes = {
+				{
+					filter = {
+						event = 'msg_show',
+						any = {
+							{ find = '%d+L, %d+B' },
+							{ find = '%d+ lines, %d+ bytes written' },
+							{ find = 'line %d+ of %d+ --%d+\\%-- col %d+-%d+' },
+							{ find = 'line %d+ of %d+' },
+							{ find = '; after #%d+' },
+							{ find = '; before #%d+' },
+						},
+					},
+					view = 'mini',
+				},
+			},
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			'MunifTanjim/nui.nvim',
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			'rcarriga/nvim-notify',
+		},
+	},
 
 	-- Functionality ---------------------------------------
 
