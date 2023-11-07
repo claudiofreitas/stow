fc6ee45e75da9efa3d4bb45aa2fc0f03a6cd5e62
Claudio Freitas <claudiofreitas3@gmail.com>
2023-11-08 01:13:23 +0900

    nvim: install and configure nvim-lint plugin

--- nvim/.config/nvim/lua/lazy-plugins/init.lua
+++ nvim/.config/nvim/lua/lazy-plugins/init.lua
@@ -52,6 +52,24 @@ return {
 			},
 		},
 	},
+	{
+		'mfussenegger/nvim-lint',
+		config = function()
+			local lint = require('lint')
+			lint.linters_by_ft = {
+				javascript = { 'eslint_d' },
+				typescript = { 'eslint_d' },
+				typescriptreact = { 'eslint_d' },
+				javascriptreact = { 'eslint_d' },
+			}
+			vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter', 'InsertLeave' }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+			vim.keymap.set('n', '<Leader>l', lint.try_lint, { desc = 'Lint' })
+		end,
+	},
 
 	-- Autocomplete & Snips
 	-- https://github.com/L3MON4D3/LuaSnip
