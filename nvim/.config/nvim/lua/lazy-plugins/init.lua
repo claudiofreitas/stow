1716f674776cb9c4937396a34b83f82509ad6a59
Claudio Freitas <claudiofreitas3@gmail.com>
2023-12-20 01:19:38 +0900

    nvim: configure NeoTree git status and group empty directories

--- nvim/.config/nvim/lua/lazy-plugins/init.lua
+++ nvim/.config/nvim/lua/lazy-plugins/init.lua
@@ -242,59 +242,85 @@ return {
 			'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
 			'MunifTanjim/nui.nvim',
 		},
-		opts = {
-			sources = {
-				'filesystem',
-				'buffers',
-				'git_status',
-				'document_symbols',
-			},
-			default_component_configs = {
-				icon = {
-					folder_empty = '󰜌',
-					folder_empty_open = '󰜌',
+		-- https://github.com/nvim-neo-tree/neo-tree.nvim#quickstart
+		config = function()
+			-- vim.api.nvim_set_hl(0, 'NeoTreeGitAdded', { link = '@property' })
+			-- vim.api.nvim_set_hl(0, 'NeoTreeGitModified', { link = '@variable.builtin' })
+			-- vim.api.nvim_set_hl(0, 'NeoTreeGitDeleted', { link = '@variable.builtin' })
+			-- vim.api.nvim_set_hl(0, 'NeoTreeGitRenamed', { link = '@type' })
+
+			vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { link = '@variable.builtin' })
+			vim.api.nvim_set_hl(0, 'NeoTreeGitIgnored', { link = '@comment' })
+			vim.api.nvim_set_hl(0, 'NeoTreeGitUnstaged', { link = '@variable.builtin' })
+			vim.api.nvim_set_hl(0, 'NeoTreeGitStaged', { link = '@property' })
+			vim.api.nvim_set_hl(0, 'NeoTreeGitConflict', { link = '@variable.builtin' })
+
+			require('neo-tree').setup({
+				sources = {
+					'filesystem',
+					'buffers',
+					'git_status',
+					'document_symbols',
 				},
-				git_status = {
-					symbols = {
-						renamed = '󰁕',
-						unstaged = '󰄱',
+				default_component_configs = {
+					icon = {
+						folder_empty = '󰜌',
+						folder_empty_open = '󰜌',
+					},
+					git_status = {
+						symbols = {
+							-- Change type
+							added = '',
+							modified = '',
+							deleted = '',
+							renamed = '',
+							-- Status type
+							untracked = '?',
+							ignored = 'I',
+							unstaged = 'U',
+							staged = 'S',
+							conflict = 'UU',
+						},
 					},
 				},
-			},
-			document_symbols = {
-				kinds = {
-					File = { icon = '󰈙', hl = 'Tag' },
-					Namespace = { icon = '󰌗', hl = 'Include' },
-					Package = { icon = '󰏖', hl = 'Label' },
-					Class = { icon = '󰌗', hl = 'Include' },
-					Property = { icon = '󰆧', hl = '@property' },
-					Enum = { icon = '󰒻', hl = '@number' },
-					Function = { icon = '󰊕', hl = 'Function' },
-					String = { icon = '󰀬', hl = 'String' },
-					Number = { icon = '󰎠', hl = 'Number' },
-					Array = { icon = '󰅪', hl = 'Type' },
-					Object = { icon = '󰅩', hl = 'Type' },
-					Key = { icon = '󰌋', hl = '' },
-					Struct = { icon = '󰌗', hl = 'Type' },
-					Operator = { icon = '󰆕', hl = 'Operator' },
-					TypeParameter = { icon = '󰊄', hl = 'Type' },
-					StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+				document_symbols = {
+					kinds = {
+						File = { icon = '󰈙', hl = 'Tag' },
+						Namespace = { icon = '󰌗', hl = 'Include' },
+						Package = { icon = '󰏖', hl = 'Label' },
+						Class = { icon = '󰌗', hl = 'Include' },
+						Property = { icon = '󰆧', hl = '@property' },
+						Enum = { icon = '󰒻', hl = '@number' },
+						Function = { icon = '󰊕', hl = 'Function' },
+						String = { icon = '󰀬', hl = 'String' },
+						Number = { icon = '󰎠', hl = 'Number' },
+						Array = { icon = '󰅪', hl = 'Type' },
+						Object = { icon = '󰅩', hl = 'Type' },
+						Key = { icon = '󰌋', hl = '' },
+						Struct = { icon = '󰌗', hl = 'Type' },
+						Operator = { icon = '󰆕', hl = 'Operator' },
+						TypeParameter = { icon = '󰊄', hl = 'Type' },
+						StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+					},
 				},
-			},
-			-- Add this section only if you've configured source selector.
-			source_selector = {
-				sources = {
-					{ source = 'filesystem', display_name = ' 󰉓 Files ' },
-					{ source = 'git_status', display_name = ' 󰊢 Git ' },
+				-- Add this section only if you've configured source selector.
+				source_selector = {
+					sources = {
+						{ source = 'filesystem', display_name = ' 󰉓 Files ' },
+						{ source = 'git_status', display_name = ' 󰊢 Git ' },
+					},
 				},
-			},
-			filesystem = {
-				filtered_items = {
-					visible = true,
+				filesystem = {
+					filtered_items = {
+						visible = true,
+					},
+					group_empty_dirs = true,
 				},
-			},
-			-- Other options ...
-		},
+				enable_git_status = true,
+				enable_diagnostics = false,
+				-- Other options ...
+			})
+		end,
 		keys = {
 			{
 				'<Leader>1',
