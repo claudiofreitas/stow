eeb05a97326793100c36d1e93e5d14bcf7cba57e
Claudio Freitas <claudiofreitas3@gmail.com>
2024-11-14 16:25:55 +0900

    nvim: change keymap to toggle color column from <Leader>cc to <Leader>\

--- nvim/.config/nvim/lua/setup-colorcolumn.lua
+++ nvim/.config/nvim/lua/setup-colorcolumn.lua
@@ -3,4 +3,4 @@ vim.g.claudio_current_color_column = '80'
 function ToggleColorColumn()
 	vim.g.claudio_current_color_column, vim.wo.colorcolumn = vim.wo.colorcolumn, vim.g.claudio_current_color_column
 end
-vim.keymap.set('n', '<Leader>cc', ToggleColorColumn)
+vim.keymap.set('n', '<Leader>\\', ToggleColorColumn)
