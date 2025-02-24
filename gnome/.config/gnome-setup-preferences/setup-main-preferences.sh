a371e8983ac91d2d2d1d112adebc5e8aa9c084ef
Claudio Freitas <claudiofreitas3@gmail.com>
2025-02-25 01:10:44 +0900

    gnome: dark theme

--- gnome/.config/gnome-setup-preferences/setup-main-preferences.sh
+++ gnome/.config/gnome-setup-preferences/setup-main-preferences.sh
@@ -8,9 +8,11 @@ gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
 
 gsettings set org.gnome.desktop.interface cursor-theme 'WhiteSur-cursors'
 
-gsettings set org.gnome.desktop.background picture-uri ""
-gsettings set org.gnome.desktop.background picture-uri-dark ""
-gsettings set org.gnome.desktop.background primary-color "#000000"
+gsettings set org.gnome.desktop.background picture-uri ''
+gsettings set org.gnome.desktop.background picture-uri-dark ''
+gsettings set org.gnome.desktop.background primary-color '#000000'
 
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
 gsettings set org.gnome.desktop.session idle-delay 0
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
